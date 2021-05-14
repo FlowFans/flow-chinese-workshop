667ef231e0d4a9872606d370cbf7a739eb52885d
jiachen <ariesjia00@hotmail.com>
2021-05-15 00:14:52 +0800

    add week4 work

--- teams/cola/week4_work.ts
+++ teams/cola/week4_work.ts
@@ -4,10 +4,10 @@ const contractAddress = 'c1e4f4f4c4257510';
 const contractName = 'Market';
 const eventName = 'MomentPurchased';
 
+const RPC = 'http://access.mainnet.nodes.onflow.org:9000'
 const getEvents = async () => {
-  sdk.config().put('accessNode.api', 'http://access.mainnet.nodes.onflow.org:9000');
-
-  let query = await sdk.build([
+  sdk.config().put('accessNode.api', RPC);
+  const query = await sdk.build([
     sdk.getBlock(true)
   ]);
   const pipedQuery = await sdk.pipe(query)
